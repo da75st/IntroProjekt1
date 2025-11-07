20ba5b80559efbaaa4aa95d34c9cb3e4c83cccf8
os44sa <osalbrink@gmail.com>
2025-11-07 12:08:22 +0100

    En mall till quizen och js_start

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,114 @@
+const questions = [
+    {
+        question: "quesiton number 1",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 2",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 3",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 4",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 5",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 6",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 7",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 8",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 9",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    },
+    {
+        question: "quesiton number 10",
+        answers: [
+            {text:'queistion1', correct: false},
+            {text:'queistion2', correct: true},
+            {text:'queistion3', correct: false},
+            {text:'queistion4', correct: false},
+        ]
+    }
+]
+
+const questionElement = document.getElementById('questionText');
+const answerbutton = document.getElementById('btn')
+const nextBtn = document.getElementById('nextbtn')
+const backBtn = document.getElementById('backbtn')
+
+let currentQuestionIndex = 0;
+let score = 0; 
+
+function showQuestion() {
+
+}
+function nextQuestion(){
+
+}
+function backQuestion(){
+
+}
+
+function showResult(){
+    
+}
