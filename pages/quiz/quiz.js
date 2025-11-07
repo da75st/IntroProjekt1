7cdecda469494404c606a7bcba88135d0cae5f8c
os44sa <osalbrink@gmail.com>
2025-11-07 18:28:03 +0100

    Fixed showquestion, nextquestion, backquestion

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,25 +1,51 @@
-// import questions from './questions.json' assert {type : 'json'};
-const questions = require('./questions.json')
-//console.log(questions);
-//console.log('h')
-
 const questionElement = document.getElementById('questionText');
-const answerbutton = document.getElementById('btn')
+const answerLabels = document.querySelectorAll('#answerButton label')
 const nextBtn = document.getElementById('nextbtn')
 const backBtn = document.getElementById('backbtn')
 
+
 let currentQuestionIndex = 0;
 
-function showQuestion() {
-
+async function importQuestions() {
+    const data = await fetch('./questions.json')
+    const questions = await data.json()
+    return questions
 }
+
+async function showQuestion() {
+    const questions = await importQuestions()
+    console.log(questions)
+    console.log(answerLabels)
+
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+
+    document.querySelectorAll('#answerButton input[type="radio"]').forEach(radio => {
+        radio.checked = false;
+    });
+
+
+    for (let i = 0; i < answerLabels.length; i++) {
+        console.log(answerLabels[i], questions[currentQuestionIndex].answers[i].text)
+        answerLabels[i].querySelector('p').textContent = questions[currentQuestionIndex].answers[i].text
+    }
+}
+
 function nextQuestion(){
-
+    currentQuestionIndex++
+    showQuestion()
 }
-function backQuestion(){
 
+function backQuestion(){
+    currentQuestionIndex--
+    showQuestion()
+    
 }
 
 function showResult(){
                                                                 
 }
+
+nextBtn.addEventListener('click', nextQuestion)
+backBtn.addEventListener('click', backQuestion)
+showQuestion()
