1e41843324ce5aab602cfa8a2c8c35d5c017c0b3
os44sa <osalbrink@gmail.com>
2025-11-27 12:53:26 +0100

    added file:// support for quiz.html
    Co-Authored-By: da75st <242410606+da75st@users.noreply.github.com>

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -10,16 +10,110 @@ const backBtnID = backBtn.id
 
 let currentQuestionIndex = 0;
 let score = 0;
-let questions = [];
+const questions = [
+    {
+        "question": "quesiton number 1",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 2",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 3",  
+        "answers": [
+            {"text":"HEJ", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 4",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 5",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 6",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 7",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 8",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 9",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    },
+    {
+        "question": "quesiton number 10",
+        "answers": [
+            {"text":"answer1", "correct": false},
+            {"text":"answer2", "correct": true},
+            {"text":"answer3", "correct": false},
+            {"text":"answer4", "correct": false}
+        ],
+        "currentGuess": null
+    }
+]
 
-async function importQuestions() {
-    const data = await fetch('./questions.json')
-    const questions = await data.json()
-    return questions
-}
-
-async function showQuestion() {
-    if (questions.length === 0) questions = await importQuestions(); 
+function showQuestion() {
     const currentQuestion = questions[currentQuestionIndex];
     questionElement.textContent = currentQuestion.question;
     const radiobuttons = document.querySelectorAll('#answerButton input[type="radio"]')
@@ -31,7 +125,6 @@ async function showQuestion() {
 
         answerLabels[i].querySelector('p').textContent = answerText
 
-        console.log(answerText, currentGuess)
         radiobuttons[i].checked = answerText === currentGuess
     }
     updateNavigationButtons(currentQuestionIndex) 
@@ -56,14 +149,13 @@ function updateNavigationButtons(currentQuestionIndex) {
 
 function handleSubmit() {
     showResult()
-    location.href = location.origin
+    location.href = getProjectRoot() + '/index.html'
 }
 
 function updateAnswer() {
     const answerElement = document.querySelector('#answerButton input[type="radio"]:checked ~ p')
     const answerText = answerElement ? answerElement.textContent : null
     questions[currentQuestionIndex].currentGuess = answerText
-    console.log(questions)
 }
 
 function guessIsCorrect(question){
@@ -85,7 +177,6 @@ function calculateScore() {
     return score
 }
 
-
 function nextQuestion(){
     updateAnswer()
     currentQuestionIndex++
@@ -99,6 +190,37 @@ function backQuestion(){
 
 function showResult(){
     alert(`Your score was: ${calculateScore()} / ${questions.length}`)                                  
+}
+
+  // Helper: gets the project root so that relative paths can be used while being served as a file://
+function getProjectRoot() {
+    const href = location.href;
+    const PROJECT_FOLDER = 'IntroProjekt1'
+    const alertMsg = `
+The website uses a web component as the header and as such is reliant on you to either load the website as a file (file:///) or so that the location.origin is valid.
+
+This is because the header (which is reused in every subpage) needs a consistant way to link to other subpages and no webserver was allowed in the project.
+
+(The root folder must be named exactly ${PROJECT_FOLDER})
+    `;
+
+    // If loaded as a file return the path
+    if (href.startsWith("file://")) {
+        const idx = href.indexOf(`/${PROJECT_FOLDER}/`);
+        if (idx !== -1) {
+            return href.slice(0, idx + PROJECT_FOLDER.length + 1);
+        }
+        alert(alertMsg);
+        return null;
+    }
+
+    // If loaded as http, return the origin
+    if (location.origin && location.origin !== "null") {
+        return location.origin;
+    } else {
+        alert(alertMsg);
+        return null;
+    }
 }
 
 nextBtn.addEventListener('click', nextQuestion)
